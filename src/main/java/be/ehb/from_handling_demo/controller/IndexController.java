c5eb86d6efc86216c577a30abed5341e4760b552
mobapp04 <mobapp04@student.ehb.be>
2019-12-11 11:54:46 +0100

    made it international and fully working

--- src/main/java/be/ehb/from_handling_demo/controller/IndexController.java
+++ src/main/java/be/ehb/from_handling_demo/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -42,5 +43,9 @@ public class IndexController {
         return "redirect:/index";
     }
 
-
+    @RequestMapping(value = "/delete/{id}" , method = RequestMethod.GET)
+    public String deleteSnack(@PathVariable(value = "id") Long id){
+        dao.deleteById(id);
+        return "redirect:/index";
+    }
 }
--- src/main/java/be/ehb/from_handling_demo/model/SnackDAO.java
+++ src/main/java/be/ehb/from_handling_demo/model/SnackDAO.java
@@ -3,5 +3,5 @@ package be.ehb.from_handling_demo.model;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SnackDAO extends CrudRepository<Snack, Long> {
-    
+
 }
